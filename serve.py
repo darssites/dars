ece5d0f5e45b3d81b615989890905fe23cbad023
gusg21 <gusg21@parkschool.net>
2017-02-16 12:57:12 -0500

    Fixed serve.py; now actually ends on Ctrl+C

--- serve.py
+++ serve.py
@@ -12,39 +12,4 @@ def serve(PORT=80):
 	try:
 		httpd.serve_forever()
 	except KeyboardInterrupt:
-		queryYesNo("End dars-make server?")
-
-def queryYesNo(question, default="yes"):
-    """
-	CREDIT TO: fmark on StackOverflow
-
-    Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
+		print("^C: Ending serve_forever()...")
